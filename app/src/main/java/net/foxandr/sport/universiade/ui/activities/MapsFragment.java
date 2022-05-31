41650c19e076ac812a11b67b47217af64b8edf97
Alex Fox <foxandr98@mail.ru>
2022-05-31 16:45:31 +0500

    обавлена статическая точка к картам

--- app/src/main/java/net/foxandr/sport/universiade/ui/activities/MapsFragment.java
+++ app/src/main/java/net/foxandr/sport/universiade/ui/activities/MapsFragment.java
@@ -34,15 +34,6 @@ public class MapsFragment extends Fragment {
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
-                /**
-                 * Manipulates the map once available.
-                 * This callback is triggered when the map is ready to be used.
-                 * This is where we can add markers or lines, add listeners or move the camera.
-                 * In this case, we just add a marker near Sydney, Australia.
-                 * If Google Play services is not installed on the device, the user will be prompted to
-                 * install it inside the SupportMapFragment. This method will only be triggered once the
-                 * user has installed Google Play services and returned to the app.
-                 */
                 LatLng urfu = new LatLng(56.84153096282171, 60.61491317053197);
                 googleMap.addMarker(new MarkerOptions().position(urfu).title("UrFU"));
                 googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(urfu, 15));
